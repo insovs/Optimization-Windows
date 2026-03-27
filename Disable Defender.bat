f55512dae9941385fb27d0a2e2f946a47fb16c98
insopti <xinaise.7ix@gmail.com>
2026-03-27 06:04:09 +0100

    Update Disable Defender.bat

--- Disable Defender.bat
+++ Disable Defender.bat
@@ -10,7 +10,7 @@ title Disable Windows Defender [https://github.com/insovs]
 echo. & echo INFO: This will disable Windows Defender and all related security services. & echo. & set /p confirm=" Disable Defender ? [y/n]: "
 if /i "%confirm%" neq "y" (echo. & echo  Aborted. & timeout /t 2 /nobreak > nul & exit /b) & echo. & echo  [*] Starting. & echo.
 
-:: ── Services ──────────────────────────────────
+:: ── Services ────────────────────────────────────────────────────────────────────
 echo  [1/6] Disabling Defender services.
 
 :: Core Defender
@@ -31,20 +31,20 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\Sense" /v "Start" /t REG_DWORD /
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\MDCoreSvc" /v "Start" /t REG_DWORD /d "4" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\MsSecCore" /v "Start" /t REG_DWORD /d "4" /f
 
-:: ── Policies ──────────────────────────────────
+:: ── Policies ────────────────────────────────────────────────────────────────────
 echo  [2/6] Disabling Defender policies.
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableRoutinelyTakingAction" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "ServiceKeepAlive" /t REG_DWORD /d "0" /f
 
-:: ── Real-Time Protection ──────────────────────
+:: ── Real-Time Protection ────────────────────────────────────────────────────────
 echo  [3/6] Disabling real-time protection.
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableIOAVProtection" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableOnAccessProtection" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d "1" /f
 
-:: ── Notifications ─────────────────────────────
+:: ── Notifications ───────────────────────────────────────────────────────────────
 echo  [4/6] Disabling notifications.
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender Security Center\Notifications" /v "DisableNotifications" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Reporting" /v "DisableEnhancedNotifications" /t REG_DWORD /d "1" /f
@@ -52,7 +52,7 @@ reg add "HKCU\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotificatio
 reg add "HKCU\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications" /v "NoToastApplicationNotificationOnLockScreen" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\UX Configuration" /v "Notification_Suppress" /t REG_DWORD /d "1" /f
 
-:: ── SmartScreen & Reporting ───────────────────
+:: ── SmartScreen & Reporting ─────────────────────────────────────────────────────
 echo  [5/6] Disabling SmartScreen and reporting.
 reg add "HKLM\SOFTWARE\Policies\Microsoft\MRT" /v "DontReportInfectionInformation" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "EnableSmartScreen" /t REG_DWORD /d "0" /f
@@ -60,7 +60,7 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\MicrosoftEdge\PhishingFilter" /v "Dont
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\SmartScreen" /v "ConfigureAppInstallControlEnabled" /t REG_DWORD /d "0" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Reporting" /v "DisableGenericReports" /t REG_DWORD /d "1" /f
 
-:: ── SpyNet & Threats ──────────────────────────
+:: ── SpyNet & Threats ────────────────────────────────────────────────────────────
 echo  [6/6] Disabling SpyNet and threat actions.
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" /v "LocalSettingOverrideSpynetReporting" /t REG_DWORD /d "0" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" /v "SpynetReporting" /t REG_DWORD /d "0" /f
